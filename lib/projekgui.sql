aec32840a1c2419c52c635ecc468933683dfe089
Kiwibiloafer <alifweq@gmail.com>
2025-02-28 09:05:49 +0700

    database update

--- lib/projekgui.sql
+++ lib/projekgui.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2025 at 03:59 AM
+-- Generation Time: Feb 28, 2025 at 03:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `projekgui`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car`
+--
+
+CREATE TABLE `car` (
+  `id_car` int(50) NOT NULL,
+  `merk` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `colour` varchar(50) NOT NULL,
+  `frame_number` varchar(50) NOT NULL,
+  `engine_number` varchar(50) NOT NULL,
+  `reg_number` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `price` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `car`
+--
+
+INSERT INTO `car` (`id_car`, `merk`, `type`, `colour`, `frame_number`, `engine_number`, `reg_number`, `status`, `price`) VALUES
+(1, 'toyota', 'zenix type q modelista', 'black', '12341213', '123143252', '12311412', 'ready', 500000),
+(2, 'honda', 'crv gen3 awd', 'red', '121345', '324567', '21345', 'ready', 300000),
+(3, 'toyota', 'raize turbo', 'white', '31456342145', '12345643214', '32456543245', 'ready', 400000);
 
 -- --------------------------------------------------------
 
@@ -57,9 +84,47 @@ INSERT INTO `employees` (`id_employees`, `name`, `username`, `password`, `positi
 (1, 'Alif Jamaludin', 'Kiwibiloafer', 'admin', 'manager'),
 (2, 'Nindita Ramya', 'ramyanindita', '1234', 'staff');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id_payment` int(20) NOT NULL,
+  `id_rent` int(50) NOT NULL,
+  `total_price` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rents`
+--
+
+CREATE TABLE `rents` (
+  `id_rent` int(50) NOT NULL,
+  `id_staff` int(50) NOT NULL,
+  `id_customer` int(50) NOT NULL,
+  `id_car` int(50) NOT NULL,
+  `rent_date` datetime NOT NULL,
+  `duration` int(50) NOT NULL,
+  `return_date` datetime NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `explanation` text NOT NULL,
+  `id_payment` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `car`
+--
+ALTER TABLE `car`
+  ADD PRIMARY KEY (`id_car`);
 
 --
 -- Indexes for table `customers`
@@ -74,8 +139,26 @@ ALTER TABLE `employees`
   ADD PRIMARY KEY (`id_employees`);
 
 --
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id_payment`);
+
+--
+-- Indexes for table `rents`
+--
+ALTER TABLE `rents`
+  ADD PRIMARY KEY (`id_rent`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `car`
+--
+ALTER TABLE `car`
+  MODIFY `id_car` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `customers`
@@ -88,6 +171,18 @@ ALTER TABLE `customers`
 --
 ALTER TABLE `employees`
   MODIFY `id_employees` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id_payment` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rents`
+--
+ALTER TABLE `rents`
+  MODIFY `id_rent` int(50) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
